542d5fb23d1a1d3638530a9fd6a39777450d3d1f
VictorDaniel07 <victor.d.santos10@aluno.senai.br>
2026-03-12 12:08:48 -0300

    lista

---
+++ atividade_03/lista_de_compras.js
@@ -0,0 +1,11 @@
+let lista_compras = ["Arroz", "Feijão", "Açucar", "Picanha"];
+lista_compras.forEach (item => {
+    console.log(item);
+}); 
+
+let cesta_frutas = ["Maçã", "Abacaxi", "Uva", "Limão"]
+cesta_frutas.push("Mamão", "Pêssego")
+cesta_frutas.forEach(fruta => {
+console.log(Limão);
+})
+
